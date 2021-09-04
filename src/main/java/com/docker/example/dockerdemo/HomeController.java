eaf8aae383f3d82e8fd2602c54f2bb5651a88b37
Prithu Adhikary <prithu.adhikary@gmail.com>
2021-09-05 00:34:17 +0530

    - to see dns based load balancing.

--- src/main/java/com/docker/example/dockerdemo/HomeController.java
+++ src/main/java/com/docker/example/dockerdemo/HomeController.java
@@ -3,12 +3,16 @@ package com.docker.example.dockerdemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 public class HomeController {
 
+    private static final String RANDOM_ID = UUID.randomUUID().toString();
+
     @GetMapping("/")
     public String hello() {
-        return "Hello World!";
+        return "Hello World from " + RANDOM_ID;
     }
 
 }
