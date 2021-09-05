5276a0a0e076a5eda7e4d80b7af3cdf2b796e8dd
Prithu Adhikary <prithu.adhikary@gmail.com>
2021-09-05 14:35:28 +0530

    - added JPA and entity and database in docker-compose.yaml.

---
+++ src/main/java/com/docker/example/dockerdemo/domain/Employee.java
@@ -0,0 +1,47 @@
+package com.docker.example.dockerdemo.domain;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    private String username;
+
+    private String password;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
